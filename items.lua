c760fc0dd3757b0e9e88388302d1b09f8968719c
Kappa Gang Scripts <126305391+Elvanoau@users.noreply.github.com>
2024-03-12 12:41:36 +1000

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,72 @@
+-- QBCore
+["Ammonite"] = {["name"] = "ammonite", ["label"] = "Ammonite", ["weight"] = 0, ["type"] = "item", ["image"] = "Ammonite.png", ["unique"] = false, ["useable"] = false, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Ammonite"},
+["Argopecten"] = {["name"] = "argopecten", ["label"] = "Argopecten", ["weight"] = 0, ["type"] = "item", ["image"] = "Argopecten.png", ["unique"] = false, ["useable"] = false, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Argopecten"},
+["Viviparus"] = {["name"] = "viviparus", ["label"] = "Viviparus", ["weight"] = 0, ["type"] = "item", ["image"] = "Viviparus.png", ["unique"] = false, ["useable"] = false, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Viviparus"},
+["Inoceramus"] = {["name"] = "inoceramus", ["label"] = "Inoceramus", ["weight"] = 0, ["type"] = "item", ["image"] = "Inoceramus.png", ["unique"] = false, ["useable"] = false, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Inoceramus"},
+["Viviparus"] = {["name"] = "viviparus", ["label"] = "Viviparus", ["weight"] = 0, ["type"] = "item", ["image"] = "Viviparus.png", ["unique"] = false, ["useable"] = false, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Viviparus"},
+["Inoceramus"] = {["name"] = "inoceramus", ["label"] = "Inoceramus", ["weight"] = 0, ["type"] = "item", ["image"] = "Inoceramus.png", ["unique"] = false, ["useable"] = false, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Inoceramus"},
+["trowel"] = {["name"] = "trowel", ["label"] = "Trowel", ["weight"] = 0, ["type"] = "item", ["image"] = "Viviparus.png", ["unique"] = false, ["useable"] = true, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Viviparus"},
+["brush"] = {["name"] = "brush", ["label"] = "Brush", ["weight"] = 0, ["type"] = "item", ["image"] = "Inoceramus.png", ["unique"] = false, ["useable"] = true, ["shouldClose"] = true, ["combinable"] = nil, ["description"] = "Inoceramus"},
+
+-- OX
+	["trowel"] = {
+		label = "Trowel",
+		weight = 100,
+		stack = false,
+		close = true,
+		description = "",
+		client = {
+			image = "trowel.png",
+		}
+	},
+	["brush"] = {
+		label = "Brush",
+		weight = 0,
+		stack = false,
+		close = true,
+		description = "",
+		client = {
+			image = "brush.png",
+		}
+	},
+
+	["ammonite"] = {
+		label = "Ammonite",
+		weight = 0,
+		stack = true,
+		close = true,
+		description = "",
+		client = {
+			image = "ammonite.png",
+		}
+	},
+	["argopecten"] = {
+		label = "Argopecten",
+		weight = 0,
+		stack = true,
+		close = true,
+		description = "",
+		client = {
+			image = "argopecten.png",
+		}
+	},
+	["viviparus"] = {
+		label = "Viviparus",
+		weight = 0,
+		stack = true,
+		close = true,
+		description = "",
+		client = {
+			image = "viviparus.png",
+		}
+	},
+	["inoceramus"] = {
+		label = "Inoceramus",
+		weight = 0,
+		stack = true,
+		close = true,
+		description = "",
+		client = {
+			image = "inoceramus.png",
+		}
+	},
